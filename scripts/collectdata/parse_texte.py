988aff3528434215338687c1ea5ebb6a9e0b553b
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-01-20 14:33:22 +0100

    typo

--- scripts/collectdata/parse_texte.py
+++ scripts/collectdata/parse_texte.py
@@ -107,7 +107,7 @@ re_mat_st  = re.compile(r"<i>\(?(non\s?-?)?(conform|modif|suppr|nouveau)", re.I)
 re_mat_new = re.compile(r"\s*\(no(n[\-\s]modifié|uveau)\s*\)\s*", re.I)
 re_clean_idx_spaces = re.compile(r'^([IVXLCDM0-9]+)\s*\.\s*')
 re_clean_art_spaces = re.compile(r'^\s*"?\s+')
-re_clean_conf = re.compile(r"^\s*\((confome|non-?modifi..?)s?\)\s*$", re.I)
+re_clean_conf = re.compile(r"^\s*\((conforme|non-?modifi..?)s?\)\s*$", re.I)
 re_clean_supr = re.compile(r'\(suppr(ession|im..?s?)\s*(conforme|maintenue|par la commission mixte paritaire)?\)["\s]*$', re.I)
 read = art_num = ali_num = 0
 section_id = ""
