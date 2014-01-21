3ebac934354058f723853440cec363ebb0015a9e
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-01-21 19:01:14 +0100

    oups commented dl; restrict to most recent texts for tryouts

--- prepare_data_promulgues.sh
+++ prepare_data_promulgues.sh
@@ -2,12 +2,13 @@
 
 mkdir -p data/.cache
 
-#curl -sL http://data.senat.fr/data/dosleg/dossiers-legislatifs.csv |
-# iconv -f "iso-8859-15" -t "utf-8" > data/.cache/list_dossiers_senat.csv
+curl -sL http://data.senat.fr/data/dosleg/dossiers-legislatifs.csv |
+ iconv -f "iso-8859-15" -t "utf-8" > data/.cache/list_dossiers_senat.csv
 
 head -n 1 data/.cache/list_dossiers_senat.csv > data/dossiers_promulgues.csv
 cat data/.cache/list_dossiers_senat.csv     |
- grep '/20\(0[8-9]\|1[0-9]\)";.*;"promulgu' |
+#grep '/20\(0[8-9]\|1[0-9]\)";.*;"promulgu' |
+ grep '/20\(50[8-9]\|1[3-9]\)";.*;"promulgu' |
  while read line; do
   url=$(echo $line | sed 's/^.*";"\(http[^"]\+\)";.*$/\1/')
   id=$(echo $url | sed 's/.*dossier-legislatif.//' | sed 's/.html$//');
