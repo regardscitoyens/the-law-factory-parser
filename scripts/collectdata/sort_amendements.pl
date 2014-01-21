3d5c5c79ac3cfc1ac3d9e3ef5b4bffb59342a286
RouxRC <b.ooghe@gmail.com>
2014-01-21 04:59:00 +0100

    use lowercase only for logic, keep and improve uppercase for display

--- scripts/collectdata/sort_amendements.pl
+++ scripts/collectdata/sort_amendements.pl
@@ -7,19 +7,21 @@ open JSON, "$textjson";
 while(<JSON>) {
     next unless (/"article"/);
     if (/"order": (\d+),.*"titre": "([^"]+)"/) {
-        $articles{$2} = $1 * 10;
+        $articles{lc($2)} = $1 * 10;
     }
 }
 close JSON;
 
 sub clean_subject {
     $subj = shift;
-    $subj =~ s/art(\.|icle|\s)*(\d+)/article \2/i;
+    $subj =~ s/art(\.|icle|\s)*(\d+)/Article \2/i;
     $subj =~ s/\(.*//;
-    $subj =~ s/(apr\S+s|avant)\s+article/\1 l'article/i;
+    $subj =~ s/(apr\S+s|avant)\s+Article/\1 l'article/i;
     $subj =~ s/\s*$//;
     $subj =~ s/^\s*//;
-    $subj = lc($subj);
+    $subj =~ s/^(.)/\U\1/;
+    $subj =~ s/(\d+e?r? )([a-z]{1,2})$/\1\U\2/i;
+    $subj =~ s/(\d+e?r? \S+ )([a-z]+)$/\1\U\2/i;
     return $subj;
 }
 sub solveorder {
@@ -27,10 +29,10 @@ sub solveorder {
     $art =~ s/premier/1er/i;
     $art =~ s/unique/1er/i;
     if ($art =~ /article (\d.*)/i) {
-        $order = $articles{$1};
-        if ($art =~ /avant/) {
+        $order = $articles{lc($1)};
+        if ($art =~ /avant/i) {
             $order--;
-        } elsif ($art =~ /apr\S+s/) {
+        } elsif ($art =~ /apr\S+s/i) {
             $order++;
         }
     }
