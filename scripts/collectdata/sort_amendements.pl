d9eaaf486c0c92e1cc0455b8146a126c5b00ebd8
Tangui Morlier <tangui@tangui.eu.org>
2014-01-22 00:55:00 +0100

    Better recognition of article orders

--- scripts/collectdata/sort_amendements.pl
+++ scripts/collectdata/sort_amendements.pl
@@ -6,26 +6,34 @@ $outputtype = shift;
 open JSON, "$textjson";
 while(<JSON>) {
     next unless (/"article"/);
-    if (/"order": (\d+),.*"titre": "([^"]+)"/) {
-        $articles{lc($2)} = $1 * 10;
+    if (/"titre": "([^"]+)"/) {
+	$titre = $1;
+	/order": (\d+),/;
+        $articles{lc($titre)} = $1 * 10;
     }
 }
 close JSON;
 
 sub clean_subject {
-    $subj = lc(shift);
+    $subj = shift;
+    $subj = lc($subj);
+    $subj =~ s/È/è/g;
+    $subj =~ s/premier/1er/i;
+    $subj =~ s/unique/1er/i;
     $subj =~ s/\s*\(((avant|apr).*)\)/ \1/;
     $subj =~ s/\(.*//;
     $subj =~ s/\s*$//;
     $subj =~ s/^\s*//;
-    $subj =~ s/^(\d)/Article \1/;
-    $subj =~ s/articles/Article/i;
-    $subj =~ s/art(\.|icle|\s)*(\d+)/Article \2/i;
-    $subj =~ s/^(apr\S+s|avant)\s*/Article additionnel \1 /;
+    $subj =~ s/^(\d)/article \1/;
+    $subj =~ s/articles/article/i;
+    $subj =~ s/art(\.|icle|\s)*(\d+)/article \2/i;
+    $subj =~ s/^(apr\S+s|avant)\s*/article additionnel \1 /;
     $subj =~ s/(apr\S+s|avant)\s+Article/\1 l'article/i;
-    $subj =~ s/^(.)/\U\1/;
     $subj =~ s/(\d+e?r? )([a-z]{1,2})$/\1\U\2/i;
     $subj =~ s/(\d+e?r? \S+ )([a-z]+)$/\1\U\2/i;
+    $subj =~ s/ annexe.*//i;
+    $subj =~ s/ rapport.*//i;
+    $subj =~ s/Article 1$/article 1er/i;
     return $subj;
 }
 sub solveorder {
@@ -37,9 +45,9 @@ sub solveorder {
     } elsif ($art =~ /^titre$/i || $art =~ /^intitul/i) {
         return -5;
     }
-    $art =~ s/premier/1er/i;
-    $art =~ s/unique/1er/i;
+    $order = -1;
     if ($art =~ /article (\d.*)/i) {
+	next unless ($articles{lc($1)});
         $order = $articles{lc($1)};
         if ($art =~ /avant/i) {
             $order--;
@@ -47,25 +55,22 @@ sub solveorder {
             $order++;
         }
     }
-    if (!$order) {
-        $order = -1;
-    }
     return $order;
 }
 
 while(<STDIN>) {
     if ($outputtype eq 'csv'){
         @csv = split /;/;
-        $sujet = clean_subject($csv[4]);
-        $order = solveorder($csv[4]);
-        $order = 'ordre article' if ($csv[4] eq "sujet");
-        s/;$csv[4];/;$order;$sujet;/;
+        $sujet = clean_subject($csv[6]);
+        $order = solveorder($sujet);
+        $order = 'ordre article' if ($sujet eq "sujet");
+        s/;$csv[6];/;$order;$sujet;/;
     } elsif($outputtype eq 'xml') {
 	@partialxml = ();
 	foreach $l (split/<amendement>/) {
 	    if ($l =~ /<sujet>([^<]+)<\/sujet>/) {
 		$sujet = clean_subject($1);
-		$order = solveorder($1);
+		$order = solveorder($sujet);
 		$l =~ s/<sujet>[^<]*<\/sujet>/<ordre_article>$order<\/ordre_article><sujet>$sujet<\/sujet>/;
 	    }
 	    push @partialxml, $l;
