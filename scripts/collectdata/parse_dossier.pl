17acfbd5cd2dd140a5d189f8b6df94ffd93f8061
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-01-24 16:31:19 +0100

    autofix links texte adopté commission from link of rapport

--- scripts/collectdata/parse_dossier.pl
+++ scripts/collectdata/parse_dossier.pl
@@ -92,7 +92,7 @@ while ($ok) {
 	}
       	if ($t->[1]{href} =~ /\/leg\/p/ || $p->get_text('/a') =~ /Texte/ || $t->[1]{href} =~ /conseil-constitutionnel/ || $t->[1]{href} =~ /legifrance/) {
 	    $url = $t->[1]{href};
-	    $url = "http://www.senat.fr".$url if ($url =~ /^\//) ;
+	    $url = "http://www.senat.fr".$url if ($url =~ /^\//);
 	    $texte = $p->get_text('/li');
 	    utf8::encode($texte);
 	    $enddate='';
@@ -126,6 +126,9 @@ while ($ok) {
 		$enddate = $date[$id];
 	    }elsif ($url =~ /assemblee-nationale/) {
 		$chambre = 'assemblee' if ($stade eq 'hemicycle');
+        if ($url =~ /\/rapports\/r/ && $enddate gt "2009-05-28") {
+            $url =~ s/rapports(\/r\d+)\./ta-commission\1-a0\./
+        }
 		if ($url =~ /[^0-9]0*([1-9][0-9]*)(-a\d)?\.asp$/) {
 			$idtext = $1;
 		}
