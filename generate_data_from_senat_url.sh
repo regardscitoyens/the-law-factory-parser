c077d0d63d81e7b029dc7718ded5e4e6a2e9eac6
RouxRC <b.ooghe@gmail.com>
2014-03-16 18:07:12 +0100

    allow to run with just id

--- generate_data_from_senat_url.sh
+++ generate_data_from_senat_url.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-if ! test "$1"; then 
+URLID=$1
+if ! test "$URLID"; then 
         echo "USAGE: $0 SENAT_URL"
         printf "\t SENAT_URL: senat.fr url describing the legislative process for the bill\n"
 	exit 1;
 fi
 
-if ! echo $1 | grep '^http://www.senat.fr/dossier-legislatif/\S\+.html' > /dev/null ; then
-	echo "$1 is not a senat.fr url describing the legislative process of the bill";
+if ! echo $URLID | grep '^http://www.senat.fr/dossier-legislatif/\S\+.html' > /dev/null ; then
+  if echo $URLID | grep '^p[pj][rl][0-9][0-9]-[0-9]\+' > /dev/null ; then
+    URLID="http://www.senat.fr/dossier-legislatif/$URLID.html"
+  else
+	echo "$URLID is not a senat.fr url describing the legislative process of the bill";
 	exit 2;
+  fi
 fi
 
-datadir=$(pwd)/data/$(echo $1 | sed 's/.*dossier-legislatif.//' | sed 's/.html$//');
+datadir=$(pwd)/data/$(echo $URLID | sed 's/.*dossier-legislatif.//' | sed 's/.html$//');
 
 mkdir -p $datadir
 
 cd scripts
 
-bash generate_collectdata.sh $1 $datadir && bash generate_vizudata.sh $datadir
+bash generate_collectdata.sh $URLID $datadir && bash generate_vizudata.sh $datadir
 exit $?
