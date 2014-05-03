09f263d97e9563a53cb3bd6b4b71a83751850c64
RouxRC <b.ooghe@gmail.com>
2014-05-04 00:21:05 +0200

    fix border effect

--- scripts/collectdata/parse_texte.py
+++ scripts/collectdata/parse_texte.py
@@ -279,7 +279,7 @@ for text in soup.find_all("p"):
             if srclst:
                 article["source_text"] = srclst[curtext]
             m = re_mat_art.match(line)
-            article["titre"] = re_cl_uno.sub("1er", m.group(1).strip().lower()).strip(" -'")
+            article["titre"] = re_cl_uno.sub("1er", re_cl_sec_uno.sub("1er", m.group(1).strip())).strip(" -'")
             if m.group(2) is not None:
                 article["statut"] = re_cl_par.sub("", real_lower(m.group(2))).strip()
             if section["id"] != "":
