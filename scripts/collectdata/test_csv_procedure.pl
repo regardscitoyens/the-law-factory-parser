eb36b3e07a1673df36637a777cbffba42bdd72c0
Tangui Morlier <tangui@tangui.eu.org>
2014-05-16 20:49:06 +0200

    Bug & more debug

--- scripts/collectdata/test_csv_procedure.pl
+++ scripts/collectdata/test_csv_procedure.pl
@@ -2,10 +2,11 @@
 $old = 0;
 while(<STDIN>) {
     @c = split /;/;
-    $c[13]=~s/-//;
-    $c[14]=~s/-//;
+    $c[13]=~s/-//g;
+    $c[14]=~s/-//g;
     next if ($c[6] eq 'XX');
     print "ERROR: previous\t $_" && exit 1 if ($c[13] < $old);
     print "ERROR: actual\t $_" && exit 1 if ($c[13] > $c[14]);
+    print STDERR "INFO: \t $c[13] -> $c[14]\n";
     $old = $c[14];
 }
