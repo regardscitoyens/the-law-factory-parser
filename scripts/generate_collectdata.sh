57f1d9e4cb4a592283dd513826e1296062187f75
Tangui Morlier <tangui@tangui.eu.org>
2016-04-03 02:08:44 +0200

    Re-enable the global generation

--- scripts/generate_collectdata.sh
+++ scripts/generate_collectdata.sh
@@ -7,13 +7,13 @@ cd collectdata
 
 mkdir -p "$datadir/.tmp"
 
-#perl parse_dossier.pl $url | perl correct_from_dossieran.pl > $datadir/.tmp/dossier.csv
+perl parse_dossier.pl $url | perl correct_from_dossieran.pl > $datadir/.tmp/dossier.csv
 
-#if ! cat  $datadir/.tmp/dossier.csv | perl check_dossier.pl "$url($datadir/.tmp/dossier.csv)" ; then
-#    echo "ERR: Errors in dossier.csv :(";
-#    rm $datadir/.tmp/dossier.csv
-#    exit 1;
-#fi
+if ! cat  $datadir/.tmp/dossier.csv | perl check_dossier.pl "$url($datadir/.tmp/dossier.csv)" ; then
+    echo "ERR: Errors in dossier.csv :(";
+    rm $datadir/.tmp/dossier.csv
+    exit 1;
+fi
 
 bash generate_data.sh $datadir/.tmp/dossier.csv $datadir || exit 1
 
