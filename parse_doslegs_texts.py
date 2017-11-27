2789a8a8135728635d1c336fbc1d69c9ca24acea
Damien <damien@dam.io>
2017-11-27 18:34:28 +0100

    parse_doslegs_texts: previous step can be last depot for nouv. lect

--- parse_doslegs_texts.py
+++ parse_doslegs_texts.py
@@ -23,6 +23,16 @@ def test_status(url):
 
 
 def get_previous_step(steps, curr_step_index):
+    curr_step = steps[curr_step_index]
+
+    # if 'nouv. lect depot' and last step failed, take last depot
+    if curr_step.get('stage') == 'nouv. lect.' and curr_step.get('step') == 'depot' and \
+        steps[curr_step_index-1].get('echec'):
+        print('[parse_doslegs_texts] fetching last depot instead of last non-failed text')
+        for i in reversed(range(curr_step_index)):
+            if steps[i].get('step') == 'depot':
+                return i
+
     for i in reversed(range(curr_step_index)):
         if not steps[i].get('echec'):
             return i
