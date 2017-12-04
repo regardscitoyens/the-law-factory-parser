b9d0f5ac34a590a94c30a6d0d7d9a2be07f06e9f
Damien <damien@dam.io>
2017-12-04 16:13:24 +0100

    add latest ideas for steps_as_dot

--- tools/steps_as_dot.py
+++ tools/steps_as_dot.py
@@ -1,20 +1,21 @@
 # quick script to produce a DOT file of the steps from a list of dosleg
 # use "python steps_as_dot.py <path_to_json>| dot -Tpng > steps.png" to produce the diagram
-import json, sys, os
+import json, sys, os, random
 
 
 if len(sys.argv) < 2:
     print('USAGE: `steps_as_dot.py <path_to_json>`')
     os.exit()
 
-all_senat_jo = json.load(open(sys.argv[1]))
+all_senat_jo = [x for x in json.load(open(sys.argv[1])) if len(x['steps']) > 2]
+# all_senat_jo = random.sample(all_senat_jo, 5)
 
 nodes_names_size = {}
 step_trans = {}
 steps_logs = ""
 for dos in all_senat_jo:
     last_step = ''
-    for step in dos.get('steps', []):
+    for step_i, step in enumerate(dos.get('steps', [])):
         step_name = ' • '.join((x for x in (step.get('stage'), step.get('institution'), step.get('step','')) if x))
         # step_name = step.get('stage')
         if step_name:
@@ -56,6 +57,8 @@ for name, id in nodes_names.items():
         fillcolor = '#B3E5FD'
     if 'senat' in name:
         fillcolor = '#f48fb1'
+    if 'CMP' in name:
+        fillcolor = '#FFD54F'
     dot_result += '\n %s [label="%s %s", penwidth="%d", fillcolor="%s"];' % (
         id, name, len(nodes_names_size[name]),
         len(nodes_names_size[name]) // 600 + 1,
