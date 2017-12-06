47de3acea9ba02088633e695ccc029893e41d59c
Damien <damien@dam.io>
2017-12-06 11:48:33 +0100

    steps_as_dot: show invalid steps in red

--- tools/steps_as_dot.py
+++ tools/steps_as_dot.py
@@ -1,11 +1,16 @@
 # quick script to produce a DOT file of the steps from a list of dosleg
 # use "python steps_as_dot.py <path_to_json>| dot -Tpng > steps.png" to produce the diagram
+
+# the XKCD font is available here: https://github.com/ipython/xkcd-font/tree/master/xkcd/build
 import json, sys, os, random
 
 
 if len(sys.argv) < 2:
     print('USAGE: `steps_as_dot.py <path_to_json>`')
     os.exit()
+
+procedure_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'doc/valid_procedure.json')
+procedure = json.load(open(procedure_file))
 
 all_senat_jo = [x for x in json.load(open(sys.argv[1])) if len(x['steps']) > 2]
 # all_senat_jo = random.sample(all_senat_jo, 5)
@@ -48,10 +53,20 @@ for prev, nexts in step_trans.items():
         prev_id = get_node_id(prev)
         for next, next_v in nexts.items():
             next_id = get_node_id(next)
-            dot_result += '\n   %s -> %s [label="%s", penwidth="%d"];' % (
-                prev_id, next_id, next_v, next_v // 400 + 1)
+
+            incorrect =  procedure.get(prev, {}).get(next, False) is False
+            color = '#F44336' if incorrect else '#a5a5a5'
+
+            dot_result += '\n   %s -> %s [label="%s", penwidth="%d", color="%s", fontcolor="%s"];' % (
+                prev_id, next_id, next_v, next_v // 400 + 1, color, color)
 
 for name, id in nodes_names.items():
+    # add previous step, mockup
+    # prev_id = id-1 if id > 0 else id + 1
+    # dot_result += '\n   %s -> %s [label="prec", penwidth="1", color="green", fontcolor="green"];' % (
+    #    id, prev_id)
+
+    # generate node
     fillcolor = "#f3f3f3"
     if 'assemblee' in name:
         fillcolor = '#B3E5FD'
@@ -60,7 +75,9 @@ for name, id in nodes_names.items():
     if 'CMP' in name:
         fillcolor = '#FFD54F'
     dot_result += '\n %s [label="%s %s", penwidth="%d", fillcolor="%s"];' % (
-        id, name, len(nodes_names_size[name]),
+        id,
+        name,
+        len(nodes_names_size[name]),
         len(nodes_names_size[name]) // 600 + 1,
         fillcolor)
 
