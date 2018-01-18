d353297eb539e6d407ccf8af80bc4637d92dd5b1
Damien <damien@dam.io>
2018-01-18 15:36:22 +0100

    tests: typo in failed test reporting

--- test_regressions.py
+++ test_regressions.py
@@ -40,7 +40,7 @@ def _is_same_helper(dircmp):
     if dircmp.left_only or dircmp.right_only or dircmp.diff_files or dircmp.funny_files:
         for name in dircmp.diff_files:
             left_file = os.path.join(dircmp.left, name)
-            right_file = os.path.join(dircmp.rightr, name)
+            right_file = os.path.join(dircmp.right, name)
             print('TWO FILES ARES DIFFERENT:', left_file, 'AND', right_file)
             diff = difflib.unified_diff(open(left_file).readlines(),
                 open(right_file).readlines())
