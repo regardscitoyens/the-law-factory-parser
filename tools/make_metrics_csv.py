3e102c00b4b3264d19c1ae982d9fd2adf8674b13
Damien <damien@dam.io>
2018-02-21 13:47:58 +0100

    remove "Étapes échouées" from csv metrics since it can be wrong

--- tools/make_metrics_csv.py
+++ tools/make_metrics_csv.py
@@ -203,7 +203,7 @@ HEADERS = [
     "Type de texte",
     "Type de procédure",
     "Étapes de la procédure",
-    "Étapes échouées",
+#   "Étapes échouées",
     "CMP",
     "Décision du CC",
     "Date de la décision du CC",
