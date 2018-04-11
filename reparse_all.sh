d61fb2f0cf0c31f96d4ad0e5c675933addaf3416
RouxRC <b.ooghe@gmail.com>
2018-04-11 17:55:16 +0200

    generate all kinds of graphs

--- reparse_all.sh
+++ reparse_all.sh
@@ -13,6 +13,9 @@ python tools/assemble_procedures.py $DATADIR
 python tools/make_metrics_csv.py $DATADIR
 
 python tools/steps_as_dot.py $DATADIR | dot -Tsvg > $DATADIR/steps.svg
+python tools/steps_as_dot.py $DATADIR | dot -Tpng > $DATADIR/steps.png
+python tools/steps_as_dot.py $DATADIR 1 | dot -Tsvg > $DATADIR/steps-detailed.svg
+python tools/steps_as_dot.py $DATADIR 1 | dot -Tpng > $DATADIR/steps-detailed.png
 
 for f in .htaccess HEADER.html; do
   cp {data,$DATADIR}/$f
