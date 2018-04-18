9d1f233bc597134d22e31c23608785e2ed9eb44c
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-04-18 12:51:45 +0200

    force redownload of groupes/parls every day

--- tools/download_groupes.py
+++ tools/download_groupes.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, time
 
 from lawfactory_utils.urls import download, enable_requests_cache
 
@@ -6,15 +6,16 @@ from lawfactory_utils.urls import download, enable_requests_cache
 def process(output_directory):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
+    yesterday = time.time() - 86400
     for url in "2007-2012.nosdeputes", "2012-2017.nosdeputes", "2017-2022.nosdeputes", "www.nosdeputes", "www.nossenateurs":
         dfile = '%s-groupes.json' % url
         destfile = os.path.join(output_directory, dfile)
-        if not os.path.exists(destfile):
+        if not os.path.exists(destfile) or os.path.getmtime(destfile) < yesterday:
             print('downloading', dfile)
             open(destfile, 'w').write(download("https://%s.fr/organismes/groupe/json" % url).text)
         dfile = '%s.parlementaires.json' % url
         destfile = os.path.join(output_directory, dfile)
-        if not os.path.exists(destfile):
+        if not os.path.exists(destfile) or os.path.getmtime(destfile) < yesterday:
             print('downloading', dfile)
             open(destfile, 'w').write(download("http://%s.fr/%s/json" %
                 (url, 'deputes' if 'deputes' in url else 'senateurs')).text)
