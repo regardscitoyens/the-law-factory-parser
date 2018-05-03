bed8ec44c32d3a7f9674da579450d6c311836d04
RouxRC <b.ooghe@gmail.com>
2018-05-03 14:07:06 +0200

    stats on run after full reparse

--- reparse_all.sh
+++ reparse_all.sh
@@ -21,5 +21,23 @@ for f in .htaccess HEADER.html; do
   cp {data,$DATADIR}/$f
 done
 
-mv data data.$TODAY.old
-mv $DATADIR data
+echo "Everything finished, data processed in $DATADIR"
+echo "A few stats:"
+
+echo PARSED: $(ls $DATADIR/p*/viz/procedure.json | wl) "/ "$(ls data/p*/viz/procedure.json | wl)
+echo ERROR: $(ls $DATADIR/logs/ | wl) "/ "$(ls data/logs/ | wl)
+echo
+tail -n 1 $DATADIR/logs/* | grep -v '^==>' | grep . | count | sort -rn
+echo
+echo "REGRESSIONS:"
+ls $DATADIR/logs/ | while read id; do
+  ls data/$id > /dev/null 2>&1 && echo "  <-  $id"
+done
+echo
+echo "NEWLY HANDLED:"
+ls data.20180502/ | grep '^p' | grep -v '_tmp' | while read id; do
+  ls data/logs/$id > /dev/null 2>&1 && echo "  ->  $id"
+done
+echo
+echo "Deploy built data with:"
+echo "mv data data.$TODAY.old && mv $DATADIR data"
