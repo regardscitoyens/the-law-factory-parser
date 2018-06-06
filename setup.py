c56f182be618a159a760755e12c53d3040dea614
Ronan Amicel <ronan.amicel@gmail.com>
2018-06-06 14:17:29 +0200

    Fix packaging
    Make sure that the `tlfp.tools` subpackage is installed.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import find_packages, setup
 from codecs import open
 from os import path
 
@@ -37,7 +37,7 @@ setup(
 
     keywords='scraping politics data',
 
-    packages=['tlfp'],
+    packages=find_packages(),
 
     install_requires=[
         "senapy",
