ecaaec4c4bf6eb0d555279ac12c47cd634f7db7b
RouxRC <b.ooghe@gmail.com>
2018-06-10 22:35:37 +0200

    cleaner with lambdas
    cf https://github.com/regardscitoyens/the-law-factory-parser/commit/4478db2b89ec403e4c6d6fca10819e24033691c5#commitcomment-29307122

--- tlfp/tools/download_AN_opendata.py
+++ tlfp/tools/download_AN_opendata.py
@@ -2,19 +2,17 @@ import sys
 
 from anpy.dossier_from_opendata import download_open_data_doslegs
 
-from .common import download_daily
-
-
-def download_AN_opendata(legislature):
-    def _download():
-        return download_open_data_doslegs(legislature)
-    return _download
+from tlfp.tools.common import download_daily
 
 
 def process(output_directory):
     all_data = {}
     for legislature in 14, 15:
-        all_data[legislature] = download_daily(download_AN_opendata(legislature), 'opendata_AN_dossiers_%d' % legislature, output_directory)
+        all_data[legislature] = download_daily(
+            lambda : download_open_data_doslegs(legislature),
+            'opendata_AN_dossiers_%d' % legislature,
+            output_directory
+        )
     return all_data
 
 
--- tlfp/tools/download_lois_dites.py
+++ tlfp/tools/download_lois_dites.py
@@ -2,15 +2,15 @@ import sys
 
 from legipy.services.law_service import LawService
 
-from .common import download_daily
-
-
-def download_lois_dites():
-    return {l.id_legi: l.common_name for l in LawService().common_laws()}
+from tlfp.tools.common import download_daily
 
 
 def process(output_directory):
-    return download_daily(download_lois_dites, 'lois_dites', output_directory)
+    return download_daily(
+        lambda : {l.id_legi: l.common_name for l in LawService().common_laws()},
+        'lois_dites',
+        output_directory
+    )
 
 
 if __name__ == '__main__':
