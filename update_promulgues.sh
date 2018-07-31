15d7329c88d55c0f7c0d7acfa224dc17ca3cedac
Damien <damien@dam.io>
2018-07-31 17:14:22 +0200

    add live texts parsing to the CRON task

--- update_promulgues.sh
+++ update_promulgues.sh
@@ -7,6 +7,8 @@ pyenv activate lafabrique
 DATADIR=data
 
 senapy-cli doslegs_urls --min-year=$((`date +%Y`)) | tlfp-parse-many $DATADIR --only-promulgated --quiet
+senapy-cli doslegs_urls --in-discussion | tlfp-parse-many $DATADIR --quiet
+anpy-cli doslegs_urls --in-discussion | tlfp-parse-many $DATADIR --quiet
 
 echo
 python tlfp/generate_dossiers_csv.py $DATADIR
