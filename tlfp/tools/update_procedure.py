f2ae7d4c0e340dcbb297213ed2838fd65921d585
Damien <damien@dam.io>
2018-08-29 15:32:54 +0200

    update_procedure: simplify, document and add test (#95)
    This commit started with the issue #95:
    update_procedure was adding a debats_order to a promulgation step
    
    First, I've added a test to make sure we don't have a regression.
    
    Then, the bug was fixed mainly by using "in_discussion" as the current
    step detector.
    
    While doing that, I simplified/re-organized the code.

--- tlfp/tools/update_procedure.py
+++ tlfp/tools/update_procedure.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+
+
+def add_auteur_depot(step):
+    if step.get('step', '') == 'depot' and step['debats_order'] is not None:
+        if '/propositions/' in step.get('source_url', ''):
+            step['auteur_depot'] = "Députés"
+        elif '/leg/ppl' in step.get('source_url', ''):
+            step['auteur_depot'] = "Sénateurs"
+        else:
+            step['auteur_depot'] = "Gouvernement"
+
+
+def remove_interventions_too_small(step, intervs):
+    # TODO: this warning is never triggered, maybe this code is obsolete ?
+    if step.get('has_interventions') and step['directory'] not in intervs:
+        print("[warning] [update_procedure] removing nearly empty interventions steps for", step['directory'])
+        step['has_interventions'] = False
 
 
 def process(procedure, articles, intervs={}):
+    """
+    Process procedure to
+        - add a fake `enddate` where it's useful
+        - detect the steps we want to display
+            (add a `debats_order` to those steps)
+        - add `auteur_depot`
+    """
     articles = articles['articles']
 
     good_steps = {}
@@ -13,43 +36,108 @@ def process(procedure, articles, intervs={}):
             if stepid not in good_steps:
                 good_steps[stepid] = int(s['directory'].split('_')[0])
 
-    # detect current step
-    currently_debated_step = None
-    if not procedure.get('end'):
-        for i, s in reversed(list(enumerate(procedure['steps']))):
-            if s['directory'] in good_steps:
-                break
-            if s.get('step') in ('hemicycle', 'commission'):
-                currently_debated_step = i
+    if not good_steps:
+        print('[warning] [update_procedure] no steps to display, parsing must have failed')
 
-    currentstep_found = False
-    for i, s in enumerate(procedure['steps']):
-        s['enddate'] = s.get('date') if i != currently_debated_step else ''
+    currently_debated_step = None
+    for s in procedure['steps']:
+        if s.get('in_discussion'):
+            currently_debated_step = s
+
+        if 'enddate' not in s and currently_debated_step is None:
+            s['enddate'] = s.get('date')
 
         s['debats_order'] = None
-        if 'has_interventions' in s and s['has_interventions'] and s['directory'] not in intervs:
-            print("WARNING: removing nearly empty interventions steps for %s" % s['directory'].encode('utf-8'), file=sys.stderr)
-            s['has_interventions'] = False
         if 'directory' in s:
-            if not s['enddate']:
-                if good_steps:
-                    if not currentstep_found:
-                        s['debats_order'] = max(good_steps.values()) + 1
-                        currentstep_found = True
-                # no good steps, it means the parsing failed
-                else:
-                    print('[update_procedure] no good steps, parsing must have failed')
-            else:
+            if currently_debated_step == s and good_steps:
+                s['debats_order'] = max(good_steps.values()) + 1
+            elif not currently_debated_step:
                 s['debats_order'] = good_steps.get(s['directory'], None)
-        if s.get('step', '') == 'depot' and s['debats_order'] != None:
-            if '/propositions/' in s.get('source_url', ''):
-                s['auteur_depot'] = "Députés"
-            elif '/leg/ppl' in s.get('source_url',''):
-                s['auteur_depot'] = "Sénateurs"
-            else:
-                s['auteur_depot'] = "Gouvernement"
-        for field in dict(s):
-            if field.endswith('_directory') or field.endswith('_files'):
-                del(s[field])
+
+        remove_interventions_too_small(s, intervs)
+        add_auteur_depot(s)
 
     return procedure
+
+
+if __name__ == "__main__":
+    # test: do not mark promulgation step as visible step even for
+    #       unfinished texts (#95)
+    result = process(
+        {
+            "url_jo": None,
+            "steps": [
+                {
+                    "date": "2011-12-01",
+                    "directory": "00_",
+                    "enddate": "2011-12-01",
+                    "institution": "senat",
+                    "stage": "1ère lecture",
+                    "step": "depot",
+                },
+                {
+                    "date": "2011-12-13",
+                    "directory": "02_",
+                    "institution": "senat",
+                    "stage": "1ère lecture",
+                    "step": "hemicycle",
+                },
+                {
+                    "date": None,
+                    "directory": "03_",
+                    "institution": "gouvernement",
+                    "stage": "promulgation",
+                },
+            ],
+        },
+        {
+            "articles": {
+                "1er": {
+                    "steps": [
+                        {"directory": "00_"},
+                        {"directory": "01_"},
+                        {"directory": "02_"},
+                    ]
+                }
+            }
+        },
+    )
+
+    try:
+        assert result == {
+            "steps": [
+                {
+                    "auteur_depot": "Gouvernement",
+                    "date": "2011-12-01",
+                    "debats_order": 0,
+                    "directory": "00_",
+                    "enddate": "2011-12-01",
+                    "institution": "senat",
+                    "stage": "1ère lecture",
+                    "step": "depot",
+                },
+                {
+                    "date": "2011-12-13",
+                    "debats_order": 2,
+                    "directory": "02_",
+                    "enddate": "2011-12-13",
+                    "institution": "senat",
+                    "stage": "1ère lecture",
+                    "step": "hemicycle",
+                },
+                {
+                    "date": None,
+                    "debats_order": None,
+                    "directory": "03_",
+                    "enddate": None,
+                    "institution": "gouvernement",
+                    "stage": "promulgation",
+                },
+            ],
+            "url_jo": None,
+        }
+    except Exception as e:
+        import pprint
+        pprint.pprint(result)
+        raise e
+    print("[update_procedure] TESTS OK")
