7c74ef8ef8822eb40d5e969577b72125e7ea7f77
RouxRC <b.ooghe@gmail.com>
2018-11-21 17:29:26 +0100

    account textes en cours crashed in errors stats

--- stats.sh
+++ stats.sh
@@ -7,18 +7,18 @@ fi
 
 
 echo PARSED: $(ls $DATADIR/p*/viz/procedure.json | wc -l) "/ "$(ls data/p*/viz/procedure.json | wc -l)
-echo ERROR: $(ls $DATADIR/logs/ | wc -l) "/ "$(ls data/logs/ | wc -l)
+echo ERROR: $(ls $DATADIR/logs{,-encours}/ | wc -l) "/ "$(ls data/logs{,-encours}/  | wc -l)
 echo
-find $DATADIR/logs/ -type f | while read f; do
+find $DATADIR/logs{,-encours}/ -type f | while read f; do
   tail -n 2 $f | head -1
 done | sort | uniq -c | sort -rn 
 echo
 echo "REGRESSIONS:"
-ls $DATADIR/logs/ | while read id; do
+ls $DATADIR/logs{,-encours}/ | grep . | while read id; do
   ls data/$id > /dev/null 2>&1 && echo "  <-  $id"
 done
 echo
 echo "NEWLY HANDLED:"
 ls $DATADIR/ | grep '^p' | grep -v '_tmp' | while read id; do
-  ls data/logs/$id > /dev/null 2>&1 && echo "  ->  $id"
+  ls data/logs{,-encours}/$id > /dev/null 2>&1 && echo "  ->  $id"
 done
