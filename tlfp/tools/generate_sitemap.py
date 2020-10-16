ceb636fe76e6128a052739afc6b3f0e8a4fba490
Damien <damien@dam.io>
2020-10-16 11:55:01 +0200

    sitemap: forgot url prefixes

--- tlfp/tools/generate_sitemap.py
+++ tlfp/tools/generate_sitemap.py
@@ -7,7 +7,7 @@ def generate_sitemap(doslegs, api_directory):
 		raw("""<?xml version="1.0" encoding="UTF-8"?>"""),
 		L.urlset(xmlns="http://www.sitemaps.org/schemas/sitemap/0.9") / [
 		    L.url / [
-		        L.loc / f"lafabriquedelaloi.fr/articles.html?loi={dos['id']}",
+		        L.loc / f"https://www.lafabriquedelaloi.fr/articles.html?loi={dos['id']}",
 		        L.lastmod / datetime.datetime.now().strftime('%Y-%m-%d'), # TODO: use directory last modified date
 		        L.changefreq / ("monthly" if dos.get('url_jo') else "daily"),
 		        L.priority / "1.0",
