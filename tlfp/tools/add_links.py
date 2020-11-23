eb37d14535e68e7f0c590181705674ded7d3b49b
Damien <damien@dam.io>
2020-11-23 03:26:20 +0100

    Revert "add links to articles who don't cite the code"
    This reverts commit 20e426983b1bc19006d8be5e548ad496bc1b0c52.

--- tlfp/tools/add_links.py
+++ tlfp/tools/add_links.py
@@ -41,38 +41,26 @@ def process(dos):
         for data in articles:
             if data["type"] == "article":
                 data['liens'] = []
-                prev_code = None
                 for i in range(len(data["alineas"])):
                     text = data["alineas"]["%03d" % (i+1)]
                     for candidat in metslesliens.donnelescandidats(text, 'structuré'):
-                        code = None
-
                         if 'texte' in candidat and 'relatif' not in candidat['texte']:
                             code = get_code(candidat)
                             if code and code not in dos["textes_cites"]:
                                 dos["textes_cites"].append(code)
-                        elif 'article' in candidat and prev_code:
-                            code = prev_code
 
-                        if code:
                             link_text = text[candidat['index'][0]:candidat['index'][1]]
                             link_text = re.sub(r'^(aux?|les?|la|du|des)(dite?s?)? ', '', link_text, 0, re.I)
                             link_text = re.sub(r"^l'", '', link_text, 0, re.I)
                             if not re.search(r'(même|présente?|précédente?) ', link_text ) and link_text not in data['liens']:
                                 """
+                                link = {
+                                    'texte': link_text
+                                }
                                 if 'eli_alias' in candidat:
                                     link['url'] = "https://www.legifrance.gouv.fr/" + candidat['eli_alias']
                                 """
-                                if code not in link_text.lower():
-                                    data['liens'].append({
-                                        'texte': link_text,
-                                        'texte_complet': link_text + ' ' + code,
-                                    })
-                                else:
-                                    data['liens'].append({
-                                        'texte': link_text,
-                                    })
-                                prev_code = code
+                                data['liens'].append(link_text)
         dos["textes_cites"].sort()
     return dos
 
